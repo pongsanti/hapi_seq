150629d6f6228e7ebd2bc5b2cc164bd20a435318
pongsanti <pongsanti.tanvejsilp@gmail.com>
2018-04-11 21:15:35 +0700

    hapi react views

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const Hapi = require('hapi');
+const HapiReactViews = require('hapi-react-views');
+const Vision = require('vision');
+
+require('babel-core/register')({
+  presets: ['react', 'env']
+});
 
 const server = Hapi.server({
   port: 3000,
@@ -11,11 +17,21 @@ server.route({
   method: 'GET',
   path: '/',
   handler: (request, h) => {
-    return 'Hello, world!';
+    return h.view('home');
   }
 });
 
 const init = async () => {
+  await server.register(Vision);
+
+  server.views({
+    engines: {
+      jsx: HapiReactViews
+    },
+    compileOptions: {}, // optional
+    relativeTo: __dirname,
+    path: 'views'
+  });
 
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
